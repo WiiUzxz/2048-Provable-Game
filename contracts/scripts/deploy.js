4c577eb0d1c76efdcb13c72019ba639bcff308d0
WiiUzxz <regelmadz022916@gmail.com>
2025-08-24 16:59:42 +0800

    Update deploy.js

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -1,1 +1,9 @@
+async function main() {
+const Leaderboard = await ethers.getContractFactory('Leaderboard');
+const leaderboard = await Leaderboard.deploy();
+await leaderboard.deployed();
+console.log('Leaderboard deployed to:', leaderboard.address);
+}
 
+
+main().catch(console.error);
