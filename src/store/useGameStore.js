44856e3d9572686c9c014dcb13dc344990a46688
WiiUzxz <regelmadz022916@gmail.com>
2025-08-24 17:15:34 +0800

    Delete src directory

--- src/store/useGameStore.js
+++
@@ -1,9 +0,0 @@
-import create from 'zustand';
-
-
-export const useGameStore = create(set => ({
-grid: Array(16).fill(0),
-score: 0,
-playHistory: [],
-// Functions: move, merge, addTile, reset
-}));
