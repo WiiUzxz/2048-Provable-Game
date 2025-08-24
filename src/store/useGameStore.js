10f40b41bf6852ee62c82a9e67c23557f225018a
WiiUzxz <regelmadz022916@gmail.com>
2025-08-24 16:57:23 +0800

    Update useGameStore.js

--- src/store/useGameStore.js
+++ src/store/useGameStore.js
@@ -1,1 +1,9 @@
+import create from 'zustand';
 
+
+export const useGameStore = create(set => ({
+grid: Array(16).fill(0),
+score: 0,
+playHistory: [],
+// Functions: move, merge, addTile, reset
+}));
